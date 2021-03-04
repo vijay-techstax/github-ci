d4e4ea4fbe6940bc0e4e4d878037b3b3375fbba7
Vijay <79891920+vijay-techstax@users.noreply.github.com>
2021-03-04 18:43:49 +0530

    Revert "v0.0.1 into main (#1)" (#4)
    This reverts commit 9b740c69eb2aba6c8ee7b1b6ba53aba40b2f0327.

--- __version__.py
+++
@@ -1,5 +0,0 @@
-__title__ = 'techstax-communications'
-__description__ = 'The Techstax communications package'
-__version__ = '0.0.1'
-__author__ = 'Techstax'
-__author_email__ = 'vijay@techstax.ml'
--- setup.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/sh
-
-cp .env.example .env
-cp environment/dev/settings_config.example environment/dev/settings_config.py
-cp environment/staging/settings_config.example environment/staging/settings_config.py
-cp environment/prod/settings_config.example environment/prod/settings_config.py
